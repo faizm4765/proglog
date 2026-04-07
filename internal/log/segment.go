b1c6cb0691ef3dc7706688481c9c64440c4fab34
faizmohammad <faizmohammad@microsft.com>
2026-04-07 19:54:58 +0000

    update params to newIndex() and newStore() methods

--- internal/log/segment.go
+++ internal/log/segment.go
@@ -1,18 +1,42 @@
 package log
 
-type segment struct{
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
+type segment struct {
 	store *store
-	index *index
+	index *Index
 }
 
 // segment whihch wraps index and store
-func newSegment(dir string) (*segment, error){
-	s := &segment{
+func newSegment(dir string, baseOffset int64) (*segment, error) {
+	s := &segment{}
 
+	var err error
+
+	storeFile, err := os.OpenFile(path.Join(dir, fmt.Sprintf("%d%s", baseOffset, ".store")), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, err
 	}
 
-	s.store = newStore()
-	s.index = newIndex()
+	s.store, err = newStore(storeFile)
+	if err != nil {
+		return nil, err
+	}
+
+	indexFile, err := os.OpenFile(path.Join(dir, fmt.Sprintf("%d%s", baseOffset, ".index")), os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	var maxIndexBytes uint64 = 1024 * 1024 // 1MB
+	s.index, err = newIndex(indexFile, maxIndexBytes)
+	if err != nil {
+		return nil, err
+	}
 
 	return s, nil
 }
